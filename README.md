f9e0de7f9837574371e3be5993ddf8fbf510c9c4
lucas <lucasgr7@gmail.com>
2021-03-19 16:09:39 -0300

    chore: estruturação projeto

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # libmp
+
+Instalação:
+
+```
+virtualenv env
+pip install -r requirements.txt
+```
+
+Gerar nova build
+
+```
+python setup.py sdist
+twine upload dist/*
+```
+
+
--- setup.py
+++ setup.py
@@ -1,22 +1,19 @@
 from distutils.core import setup
 setup(
   name = 'libmp',
-  packages = ['LIBMP'],
+  packages = ['libmp'],
   version = '0.1',
   license='MIT',
   description = 'LIBRARY FOR SUPPORT FUNCTIONS AND METHODS AT WORK FOR GROUP MULTIPECAS FROM BRAZIL',
   author = 'LUCAS GARCIA RIBEIRO',
   author_email = 'lucasgr7@gmail.com',
   url = 'https://onlinemotopecas.com.br/',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  download_url = 'https://github.com/lucasgr7/libmp/archive/refs/tags/v0.1.tar.gz',
   keywords = ['LIBRARY', 'MULTIPECAS', 'API', 'MERCADOLIVRE'],
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+  #install_requires=[],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 2.7',
